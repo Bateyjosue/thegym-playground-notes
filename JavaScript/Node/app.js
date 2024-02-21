33202326dfa30f1a1b9f16d39cf4d554dc9b583e
Bateyjosue <josuebatey19@gmail.com>
2024-02-21 20:04:48 +0200

    add collection and schema for skills

--- JavaScript/Node/app.js
+++ JavaScript/Node/app.js
@@ -1,16 +1,54 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express()
+
+const Skill = require('./models/skills')
 
 // View engine configuration
 app.set('view engine', 'ejs')
 app.use(express.static("public"));
 app.use(express.static("node_modules"));
 
-app.listen(3000)
+// DB Connection
 
-app.get('/', (req, res) => {
-  const stacks = ['TypeScript', 'Javascript', 'Nodejs', 'TailwindCss']
+const dbURI = 'mongodb+srv://josuebatey19:mdpNMoWT3Sgaccsa@cluster0.hs7ibqw.mongodb.net/node-blog?retryWrites=true&w=majority'
+
+mongoose.connect(dbURI)
+  .then(res => app.listen(3000))
+  .catch(err => console.log(err))
+
+// Define a Schema
+
+
+
+app.get('/', async (req, res) => {
+  const stacks = []
+
+  // // Create a document
+  // const skill = new Skill({
+  //   title: 'NestJS',
+  //   link: 'https://www.nestjs.org',
+  //   icon: 'fa fa-home',
+  // })
+
+  // try {
+  //   const skillData = await skill.save()
+  //   console.log(skillData);
+  // }
+  // catch (err) { 
+  //   console.log(err);
+  // }
+
+  // get all document
+
+  try {
+
+    const allSkills = await Skill.find()
+    allSkills.forEach(skill => stacks.push(skill))
+  } catch (err) { 
+    console.log(err);
+  }
 
   res.render('index', { title: 'Home', stacks})
 })
